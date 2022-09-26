a53e093ae358c729972e600ac7221002ee330a22
nkusanda1 <natkusanda@gmail.com>
2022-09-26 16:45:58 -0400

    add code for remaining plots

--- compile_data.py
+++ compile_data.py
@@ -20,6 +20,7 @@ import plotly.graph_objects as go
 import matplotlib.pyplot as plt
 from tqdm import tqdm
 import seaborn as sns
+from cairosvg import svg2png
 # %%
 # %% Reading Pareto-tagged zinc dataset
 df = pd.read_csv("pareto_tagged_logp_qed.csv")
@@ -40,7 +41,9 @@ w_ref = np.array([-logp_worst,-qed_worst])
 hypervolume = get_hypervolume(pareto_front, w_ref)
 print(hypervolume)
 # %%
-# Task 1: getting hypervolumes, retain 10% percentile worst molecules
+'''
+Task 1: getting hypervolumes, retain 10% percentile worst molecules
+'''
 properties = ['logp', 'qed']
 objectives = ['max', 'max']
 kind = 'Hypervolume'
@@ -59,10 +62,12 @@ for i in range(len(scal_vals)):
         hv_keep_smiles.append(smiles[i])
 hv_keep_data = np.array(hv_keep_data)
 # %%
-with open(pwd + "zinc_250k_sliced.txt", "w") as f:
+with open("zinc_250k_sliced.txt", "w") as f:
     f.writelines(["{}".format(x) for x in hv_keep_smiles])
 # %%
-# Task 2: Getting minimum distances to utopian point
+'''
+Task 2: Getting minimum distances to utopian point
+'''
 def gen_min_all(optim_type):
     pwd = "/RESULTS_" + optim_type 
     min_distances = []
@@ -207,7 +212,7 @@ rand_min[7] = gen_min_all("random8")
 rand_min[8] = gen_min_all("random9")
 rand_min[9] = gen_min_all("random10")
 rand_mindist = stack_mean_std(rand_min)
-# %%
+# %% Plotting minimum distance
 def plot_shade(y1,y2,y3,y4,ext):
     t = np.arange(1, 101, 1)
     fig = plt.figure(figsize=(10,8))
@@ -225,22 +230,23 @@ def plot_shade(y1,y2,y3,y4,ext):
     ax.fill_between(t, y3[:,0] - y3[:,1], y3[:,0] + y3[:,1], color='C2', alpha=0.4)
     ax.fill_between(t, y4[:,0] - y4[:,1], y4[:,0] + y4[:,1], color='C3', alpha=0.4)
 
-    ax.set_ylabel('Minimum Distance') 
-    ax.set_xlabel('Generation')
+    ax.set_ylabel('Minimum Distance', fontsize=25) 
+    ax.set_xlabel('Generation', fontsize=25)
     ax.grid(True, linestyle='-.')
     ax.tick_params(labelcolor='black', labelsize='medium', width=3)
     plt.xticks(np.arange(0, 101, 10))
     plt.title("Minimum Distance to Utopian Point")
     plt.legend(['Control','Random','Chimera','Hypervolume'], 
-        loc = 'best', prop={'size': 12})
+        loc = 'best', prop={'size': 25})
     plt.savefig('utopia_' + ext + '.jpg', dpi = 400)
 # %%
 plot_shade(ctrl_mindist,rand_mindist,chim_mindist,hv_mindist,'1')
 # %%
 # %%
-# Task 3: Plotting hypervolumes of Pareto fronts
+'''
+Task 3: Plotting hypervolumes of Pareto fronts
+'''
 df = pd.read_csv("all_hvs.csv")
-#df = pd.read_csv(pwd + "250k_rndm_zinc_drugs_clean_3.csv")
 all_hv = np.empty((10,4))
 all_hv[0] = list(df['1'])[:4]
 # %%
@@ -259,14 +265,208 @@ volumes = np.row_stack((all_hv[:,0],all_hv[:,1],all_hv[:,2],all_hv[:,3]))
 types = np.row_stack((["ctrl"]*10,["random"]*10,["chimera"]*10,["hv"]*10))
 df["hypervolume"] = volumes.flatten()
 df["type"] = types.flatten()
-# %%
+# %% Violin plot
 dframe = pd.DataFrame(data=df)
 fig = plt.figure(figsize=(10,10))
 ax = fig.add_subplot(111)
 sns.violinplot(data=dframe, x="type", y="hypervolume")
+plt.axhline(y=12.40,color='black', label = 'ZINC_red average',linestyle='--')
+ax.annotate('ZINC_red',xy=(2.4,12.45),fontsize=20)
 plt.ylabel('Hypervolume', fontsize=20)
 plt.xlabel('Approach', fontsize=20)
+plt.xticks(rotation = 15)
 plt.title("Hypervolume of Pareto Front", fontsize=20)
 ax.tick_params(axis='both', which='major', labelsize=20)
-plt.savefig(pwd + 'hv_of_pareto_violin.jpg')
+plt.savefig('hv_of_pareto_violin.png')
 # %%
+'''
+Task 4: Plotting closest molecules to utopian point
+'''
+def top_min_all(optim_type):
+    pwd = "/RESULTS/"
+    min_distances = []
+
+    min_qed = []
+    min_logp = []
+    min_dist = []
+    min_smi = []
+
+    for i in tqdm(range(1,11)):
+        
+        if i == 1:
+            df = pd.read_csv(pwd + "smiles_collector_" + optim_type + ".csv")
+        else:
+            df = pd.read_csv(pwd + "smiles_collector_" + optim_type + str(i) + ".csv")
+
+
+        smiles = list(df['smi'])
+        logp = list(df['logp'])
+        qed = list(df['qed'])
+
+        data = np.column_stack((qed,logp))
+        utopia = np.array([0.6,10])
+        dist_1 = (utopia[0] - data[:,0]) / utopia[0]
+        dist_2 = (utopia[1] - data[:,1]) / utopia[1]
+
+        dist_1 = np.array([dist_1[i] if dist_1[i]>0 else 0 for i in range(len(dist_1))])
+        dist_2 = np.array([dist_2[i] if dist_2[i]>0 else 0 for i in range(len(dist_2))])
+
+        all_dist = np.sqrt(dist_1 ** 2 + dist_2 ** 2)
+
+        sort_by_dist_idx = np.argsort(all_dist)
+
+        sort_qed = np.array(qed)[sort_by_dist_idx]
+        sort_logp = np.array(logp)[sort_by_dist_idx]
+        sort_dist = np.array(all_dist)[sort_by_dist_idx]
+        sort_smi = np.array(smiles)[sort_by_dist_idx]
+
+        min_qed.extend(list(sort_qed[:10]))
+        min_logp.extend(list(sort_logp[:10]))
+        min_dist.extend(list(sort_dist[:10]))
+        min_smi.extend(list(sort_smi[:10]))
+    
+    final_sort_idx = np.argsort(min_dist)
+    top_qed = np.array(min_qed)[final_sort_idx]
+    top_logp = np.array(min_logp)[final_sort_idx]
+    top_dist = np.array(min_dist)[final_sort_idx]
+    top_smi = np.array(min_smi)[final_sort_idx]
+
+    return top_qed[:30], top_logp[:30], top_dist[:30], top_smi[:30]
+# %%
+chim_three = np.empty((3,30))
+chim_three[0],chim_three[1],chim_three[2],chim_smiles = top_min_all('chimera')
+# %%
+ctrl_three = np.empty((3,30))
+ctrl_three[0],ctrl_three[1],ctrl_three[2],ctrl_smiles = top_min_all('ctrl')
+# %%
+random_three = np.empty((3,30))
+random_three[0],random_three[1],random_three[2],rand_smiles = top_min_all('random')
+# %%
+hv_three = np.empty((3,30))
+hv_three[0],hv_three[1],hv_three[2],hv_smiles = top_min_all('hv')
+# %%
+def top_zinc_pareto_points():
+    min_distances = []
+
+    min_qed = []
+    min_logp = []
+    min_dist = []
+    min_smi = []
+
+    df = pd.read_csv("pareto_tagged_logp_qed.csv")
+
+
+    smiles = list(df['smiles'])
+    logp = list(df['logP'])
+    qed = list(df['qed'])
+    pareto = list(df['pareto'])
+
+    qed_pareto = [qed[i] for i in range(len(smiles)) if pareto[i] == 1]
+    logp_pareto = [logp[i] for i in range(len(smiles)) if pareto[i] == 1]
+    smiles_pareto = [smiles[i] for i in range(len(smiles)) if pareto[i] == 1]
+
+    data = np.column_stack((qed_pareto,logp_pareto))
+    utopia = np.array([0.6,10])
+    dist_1 = (utopia[0] - data[:,0]) / utopia[0]
+    dist_2 = (utopia[1] - data[:,1]) / utopia[1]
+
+    dist_1 = np.array([dist_1[i] if dist_1[i]>0 else 0 for i in range(len(dist_1))])
+    dist_2 = np.array([dist_2[i] if dist_2[i]>0 else 0 for i in range(len(dist_2))])
+
+    all_dist = np.sqrt(dist_1 ** 2 + dist_2 ** 2)
+
+    sort_by_dist_idx = np.argsort(all_dist)
+    
+    sort_qed = np.array(qed_pareto)[sort_by_dist_idx]
+    sort_logp = np.array(logp_pareto)[sort_by_dist_idx]
+    sort_dist = np.array(all_dist)[sort_by_dist_idx]
+    sort_smi = np.array(smiles_pareto)[sort_by_dist_idx]
+
+    min_qed.extend(list(sort_qed[:30]))
+    min_logp.extend(list(sort_logp[:30]))
+    min_dist.extend(list(sort_dist[:30]))
+    min_smi.extend(list(sort_smi[:30]))
+    
+
+    return min_qed,min_logp,min_dist,min_smi
+# %%
+zinc_pareto = np.empty((3,30))
+zinc_pareto[0],zinc_pareto[1],zinc_pareto[2],zinc_smi = top_zinc_pareto_points()
+# %%
+df = {}
+df['qed'] = np.concatenate((ctrl_three[0],random_three[0],chim_three[0],hv_three[0]))
+df['logp'] = np.concatenate((ctrl_three[1],random_three[1],chim_three[1],hv_three[1]))
+df['type'] = np.concatenate((['WeightedSum']*30,['Random']*30,['Chimera']*30,['Hypervolume']*30))
+utop_df = {}
+utop_df['qed'] = [0.6]
+utop_df['logp'] = [10]
+utop_df['type'] = ['Utopian point']
+pareto_df = {}
+pareto_df['qed'] = zinc_pareto[0]
+pareto_df['logp'] = zinc_pareto[1]
+pareto_df['type'] = ['Zinc'] * 30
+# %%
+def good_plot(ml_df, utop_df, pareto_df, arg1, arg2, ext, xlims, ylims):
+    fig = plt.figure(figsize=(8,8))
+    ax = fig.add_subplot(111)
+
+    ax.set_xlim(xlims)
+    ax.set_ylim(ylims)
+    ax.axhline(0, linestyle="-", color='black')
+    ax.axvline(0, linestyle="-", color='black')
+    plt.title("Plot of molecules closest to utopian point")
+
+    markers = {"Zinc": 's',"Chimera": 'o',"WeightedSum": 'o',
+        "Random": 'o',"Hypervolume": 'o',"Utopian point": 'X'}
+    sns.scatterplot(data=ml_df, x=arg1, y=arg2, hue ='type', 
+           legend ='full',style='type',markers=markers,s=60)
+    sns.scatterplot(data=utop_df, x=arg1, y=arg2, hue='type',palette='gist_gray_r',
+           legend ='full',style='type',markers=markers,s=500)
+    sns.lineplot(data=pareto_df, x=arg1, y=arg2, hue ='type',palette='Purples', sizes=(50,50),
+           legend ='full',style='type',markers=markers,markersize=10)
+    plt.legend(loc = 'best', prop={'size': 20})
+    ax.tick_params(axis='both', which='major', labelsize=20)
+    ax.tick_params(axis='both', which='minor', labelsize=20)
+
+    ax.set_ylabel('QED', fontsize=20) 
+    ax.set_xlabel('logP', fontsize=20)
+    
+    props = dict(boxstyle='round', facecolor='wheat', alpha=0.5)
+
+    plt.savefig("utopia" + arg1 + '_' + arg2 + '_' + ext + '.png',dpi = 800)
+# %%
+ml_df = pd.DataFrame(df)
+u_df = pd.DataFrame(utop_df)
+p_df = pd.DataFrame(pareto_df)
+good_plot(ml_df, u_df, p_df, 'logp','qed','close_plot',[4,10.5],[0.3,0.9])
+
+# %%
+'''
+Task 5: Visualising generated molecules
+'''
+# %%
+def visualise_grid(optim_smiles,optim_data,optim_type):
+    subms = [Chem.MolFromSmiles(x) for x in optim_smiles]
+    legends = []
+    for i in range(len(optim_smiles)):
+        
+        qed_str = "QED: {:.2f}".format(optim_data[0][i])
+        logp_str = "logP: {:.2f}".format(optim_data[1][i])
+
+        #legend = chim_smiles[i] + "\n" * 5 + qed_str + "\n" * 5 + logp_str
+        legend = qed_str + "\n" * 5 + logp_str
+        legends.append(legend)
+    
+    d2d = Chem.Draw.MolDraw2DSVG(600 * 5, 600 * 2, 600, 600)
+    d2d.drawOptions().addStereoAnnotation=True
+    d2d.drawOptions().legendFontSize = 55
+    d2d.drawOptions().legendFraction = 0.2
+    d2d.DrawMolecules(subms[:10],legends=legends[:10])
+    d2d.FinishDrawing()
+    svg_string = d2d.GetDrawingText()
+    svg2png(bytestring=svg_string,write_to=optim_type+'_pop.png')
+# %%
+visualise_grid(chim_smiles,chim_three,'chimera')
+visualise_grid(ctrl_smiles,ctrl_three,'ctrl')
+visualise_grid(rand_smiles,random_three,'random')
+visualise_grid(hv_smiles,hv_three,'hv')
